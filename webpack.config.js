4279bb1741979a999cdd7b2303f60810af726ec8
Mary Gore <dm101mary@gmail.com>
2022-05-08 00:27:02 +0400

    update files

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,21 @@ module.exports = {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'build')
   },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env']
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
   context: path.resolve(__dirname, 'src'),
   plugins: [
     new webpack.ProvidePlugin({
