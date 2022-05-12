7dcdad215d843b09ab56f163f7dba289730d5444
Mary Gore <dm101mary@gmail.com>
2022-05-13 00:56:49 +0400

    update files

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'docs'),
-    publicPath: 'https://mary-gore.github.io/landscape',
     environment: {
       arrowFunction: false
     }
